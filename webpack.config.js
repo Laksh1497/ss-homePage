e9ea1c19a6c2d6322a9c675e3f1864d5f0d1a066
laksh@amplifai <91502957+Laksh1497@users.noreply.github.com>
2021-10-05 14:26:45 -0500

    changed webconfig cors headers

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (webpackConfigEnv, argv) => {
     // modify the webpack config however you'd like to by adding to this object
     devServer: {
       headers: {
-        "Access-Control-Allow-Origin": "https://main.d2rnselr68b9dq.amplifyapp.com",
+        "Access-Control-Allow-Origin": "https://main.d2rnselr68b9dq.amplifyapp.com/darthlaksh97-root-config.js",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
         "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
       }
